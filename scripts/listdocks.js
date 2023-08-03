cf97c7933e15c1d71d0be8df3534cce6c25c4205
Sylvia T <sylviarosemarie@gmail.com>
2023-08-03 16:24:19 -0500

    created functions to alert user re which ships are pulling which ships upon click on either haulers or cargo ships

--- scripts/listdocks.js
+++ scripts/listdocks.js
@@ -2,9 +2,9 @@ import { getDocks } from "./database.js";
 
 const docks = getDocks();
 export const dockList = () => {
-  let docksHTML = "<ul>";
+  let docksHTML = `<ul id="docks">`;
   for (const dock of docks) {
-    docksHTML += `<li> The dock at ${dock.location} can hold ${dock.volume} million tons of cargo </li>`;
+    docksHTML += `<li data-id="${dock.id}" data-type="dock"> The dock at ${dock.location} can hold ${dock.volume} million tons of cargo </li>`;
   }
   docksHTML += "</ul>";
   return docksHTML;
--- scripts/listhaulers.js
+++ scripts/listhaulers.js
@@ -1,11 +1,32 @@
 import { getHaulers } from "./database.js";
+import { getShips } from "./database.js";
 
 const haulers = getHaulers();
+const shippingShips = getShips();
+
 export const haulerList = () => {
-  let haulerHTML = "<ul>";
+  let haulerHTML = `<ul id="haulers">`;
   for (const hauler of haulers) {
-    haulerHTML += `<li>${hauler.name}</li>`;
+    haulerHTML += `<li 
+    data-id="${hauler.id}" 
+    data-type="hauler"
+    data-name="${hauler.name}">
+    ${hauler.name}</li>`;
   }
   haulerHTML += "</ul>";
   return haulerHTML;
 };
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+  let counter = 0;
+  if (itemClicked.dataset.type === "hauler") {
+    const haulerID = itemClicked.dataset.id;
+    for (const ship of shippingShips) {
+      if (parseInt(haulerID) === ship.haulerid) {
+        counter++;
+      }
+    }
+    window.alert(`This hauler is carrying ${counter} shipping ships.`);
+  }
+});
