3ddb42a600bcbcd7c5ed636161326e260539c2a2
Sylvia T <sylviarosemarie@gmail.com>
2023-08-04 09:57:52 -0500

    committing small changes to clean up before pulling a new branch for the mess to come

--- scripts/listhaulers.js
+++ scripts/listhaulers.js
@@ -3,6 +3,7 @@ import { getShips } from "./database.js";
 
 const haulers = getHaulers();
 const shippingShips = getShips();
+const haulerArray = [];
 
 export const haulerList = () => {
   let haulerHTML = `<ul id="haulers">`;
@@ -25,8 +26,13 @@ document.addEventListener("click", (clickEvent) => {
     for (const ship of shippingShips) {
       if (parseInt(haulerID) === ship.haulerid) {
         counter++;
+        haulerArray.push(ship.name);
       }
     }
     itemClicked.innerHTML = `This hauler is carrying ${counter} shipping ships.`;
   }
 });
+
+export const getHaulerArray = () => {
+  return haulerArray;
+};
