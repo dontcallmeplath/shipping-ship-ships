3b9afd866a339a4bcc0ad544ca08511a48ad9582
Sylvia T <sylviarosemarie@gmail.com>
2023-08-07 14:47:55 -0500

    created function to sort each list of names alphabetically

--- scripts/listhaulers.js
+++ scripts/listhaulers.js
@@ -3,10 +3,52 @@ import { getShips } from "./database.js";
 
 const haulers = getHaulers();
 const shippingShips = getShips();
+const newArray = [];
+
+const alphabetizeArrayOfHaulers = (haulersArray) => {
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  for (const letter of alphabet) {
+    haulersArray.forEach((hauler) => {
+      if (hauler.name.toLowerCase().startsWith(letter)) {
+        newArray.push(hauler);
+      }
+    });
+  }
+  return newArray;
+};
+
+alphabetizeArrayOfHaulers(haulers);
 
 export const haulerList = () => {
   let haulerHTML = `<ul id="haulers">`;
-  for (const hauler of haulers) {
+  for (const hauler of newArray) {
     haulerHTML += `<li 
     data-id="${hauler.id}" 
     data-type="hauler"
